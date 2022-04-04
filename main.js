28daf237174080dc35994e5de4fed32849617217
chelle tuerk <tuerkbot@talkingRobot.local>
2022-04-03 19:38:32 -0700

    major styling changes

--- main.js
+++ main.js
@@ -1,106 +1,85 @@
-const vgsForm = window.VGSCollect.create('tntv3bfscmb', 'SANDBOX', () => {});
+const f = window.VGSCollect.create('tntv3bfscmb', 'SANDBOX', () => {});
+// vgsForm.useCname('tntv3bfscmb.sandbox.verygoodproxy.com')
 
-const css = {
-  boxSizing: 'border-box',
-  fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI"',
+const styles = {
+  border: "none",
+  background: "rgba(215, 224, 235, 0.18);",
+  height: "40px",
+  lineHeight: "normal",
+  padding: "0 10px",
+  color: "white",
+  fontSize: "12px",
+  boxSizing: "border-box",
+  borderRadius: "4px",
+  letterSpacing: ".7px",
+  "&::placeholder": {
+    color: "white",
+    fontSize: "12px",
+    opacity: ".5"
+  }
 };
 
-vgsForm.field('#cc-holder', {
-  type: 'text',
-  name: 'card_holder',
-  placeholder: 'Card holder',
-  validations: ['required'],
-  css: css,
+f.field("#cc-holder .field-space", {
+  type: "text",
+  name: "card.name",
+  placeholder: "Name on card",
+  validations: ["required"],
+  css: styles
 });
 
-vgsForm.field('#cc-number', {
-  type: 'card-number',
-  name: 'card_number',
-  successColor: '#4F8A10',
-  errorColor: '#D8000C',
-  placeholder: 'Card number',
-  showCardIcon: true,
-  validations: ['required', 'validCardNumber'],
-  css: css,
+f.field("#cc-number .field-space", {
+  type: "card-number",
+  name: "card.number",
+  placeholder: "Card number",
+  validations: ["required", "validCardNumber"],
+  css: styles
 });
 
-vgsForm.field('#cc-cvc', {
-  type: 'card-security-code',
-  name: 'card_cvc',
-  successColor: '#4F8A10',
-  errorColor: '#D8000C',
-  placeholder: 'CVC',
-  maxLength: 3,
-  validations: ['required', 'validCardSecurityCode'],
-  css: css,
+f.field("#cc-cvc .field-space", {
+  type: "card-security-code",
+  name: "card.cvc",
+  placeholder: "CVV",
+  validations: ["required", "validCardSecurityCode"],
+  css: styles
 });
 
-vgsForm.field('#cc-expiration-date', {
-  type: 'card-expiration-date',
-  name: 'card_exp',
-  successColor: '#4F8A10',
-  errorColor: '#D8000C',
-  placeholder: 'MM / YY',
-  validations: ['required', 'validCardExpirationDate'],
-  css: css,
+f.field("#cc-exp .field-space", {
+  type: "card-expiration-date",
+  name: "card.expirationDate",
+  placeholder: "MM / YYYY",
+  validations: ["required", "validCardExpirationDate"],
+  css: styles
 });
 
-document.getElementById("collect-form").addEventListener('submit', (e) => {
-  e.preventDefault();
-    vgsForm.submit('/post', {}, (status, response) => {
+f.on('enterPress', (field) => {
+  f.submit('/post', {}, (status, response) => {
     if (status === 200) {
+      let elem = document.getElementsByClassName("card-success")[0];
+      elem.classList.remove("hidden");
       console.log("success")
       console.log('response', response.data)
     }
   }, (error) => {
     console.log(error);
-  });
-  vgsForm.reset()
-});
-
-
-vgsForm.on('enterPress', (field) => {
-  vgsForm.submit('/post', {}, (status, response) => {
-    if (status === 200) {
-      console.log("success")
-      console.log('response', response.data)
-      vgsForm.reset()
-    }
-  }, (error) => {
-    console.log(error);
-    vgsForm.reset()
   })
+  f.reset()
 });
 
-
-
-// const form = VGSCollect.create("tntv3bfscmb", "sandbox", function(state) {
-// })
-//
-// $('#button').click((e) => {
-//   let value = $('#input').val()
-//   if (value == '') {
-//     e.stopPropagation()
-//   }
-//   // $('li').remove()
-//   // console.log($('#input').val(form))
-//   // console.log(value)
-//   $('#ul').append(`<li>${value}</li>`)
-//   $('#input').val('')
-//   form.field('#cc-number', {
-//   type: 'card-number',
-//   name: 'card_number',
-//   placeholder: 'Card number',
-//   showCardIcon: true,
-//   validations: ['required', 'validCardNumber'],
-//   css: { border: '1px solid gray' }
-//   })
-// })
-//
-// console.log('form', form)
-//
-//
-//
-// form.submit('/post', {}, (status, response) => {
-//     console.log(response);
-// })
+document.getElementById("cc-form-submit").addEventListener("click", function() {
+  f.submit(
+    "/post",
+    {
+      headers: {
+        "x-custom-header": "Oh yes. I am a custom header"
+      }
+    },
+    function(status, response) {
+      let elem = document.getElementsByClassName("card-success")[0];
+      elem.classList.remove("hidden");
+      console.log('response', response)
+      console.log("success")
+      f.reset()
+    },
+    function(errors) {console.log(error)}
+  );
+});
